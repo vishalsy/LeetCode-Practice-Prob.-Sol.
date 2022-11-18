46d40c2dd3c37ddc5a8bcb740c7261ab5a6ca79b
vishal singh yadav <100470249+vishalsy@users.noreply.github.com>
2022-11-18 22:50:13 +0530

    Time: 8 ms (39.37%), Space: 6.7 MB (49.93%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -11,14 +11,12 @@ public:
         int j=0;
         for(auto i:m){
             if(i.second%2==0) ct+=i.second;
-            else if(i.second==1)t++;
             else{ct+=i.second-1;
                  j++;
                 }
             
         }
-        if(t>0) ct+=1;
-        else if(j>0) ct+=1;
+        if(j>0) ct+=1;
         return ct;
     }
 };
